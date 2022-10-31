3c8d0043e496583ff099a7b215e4c6e490654d52
ubong <ubongnt@gmail.com>
2022-10-31 12:28:09 +0100

    commit

---
+++ Celsius_to_Fahrenheit_Converter.py
@@ -0,0 +1,6 @@
+celsius = int(input())
+def conv(c):
+    #your code goes here
+    return 9/5 * celsius + 32
+fahrenheit = conv(celsius)
+print(fahrenheit)
---
+++ book_tiles.py
@@ -0,0 +1,8 @@
+file = open("/usercode/files/books.txt", "r")
+#your code goes here
+for line in file :
+ if line[-1] == "\n" :
+  print(line[0]+str(len(line)-1))
+ else :
+  print(line[0]+str(len(line)))
+file.close()
